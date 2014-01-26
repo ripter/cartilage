1122f18ae41480450908a0dcbd58fcee014749e4
ripter <ripter001@gmail.com>
2014-01-25 18:28:15 -0800

    tests are now setup

---
+++ test/.#test-main.js
@@ -0,0 +1,1 @@
+chrisrichards@Chriss-MacBook-Pro-2.local.6282
---
+++ test/modelSpec.js
@@ -0,0 +1,7 @@
+/*global require, describe, it */
+
+describe('model', function() {
+  it('should be ok', function() {
+    true.should.be.true;
+  });
+});
